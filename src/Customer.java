1e9dfd6907d06ffa9d29acc86a9a22659aa1f9e0
TheJagermeister <pedroservano007@gmail.com>
2022-11-24 05:16:06 +0100

    Intefaz añadida

---
+++ src/Customer.java
@@ -0,0 +1,27 @@
+public interface Customer {
+    public Integer getIDCustomer();
+    public void  setIDCustomer(Integer IDCustomer);
+
+    public String getIN();
+    public void setIN(String IN);
+
+    public String getNameCust();
+    public void setNameCust(String NameCust);
+
+    public Integer getZipCode();
+    public void setZipCode(Integer ZipCode);
+
+    public String getCity();
+    public void setCity(String City);
+
+    public String getProvince();
+    public void setProvince(String Province);
+
+    public String getEmail();
+    public void setEmail(String Email);
+
+    public Integer getPhoneNumber();
+    public void setPhoneNumber(Integer PhoneNumber);
+
+
+}
---
+++ src/Sales.java
@@ -0,0 +1,18 @@
+public interface Sales {
+
+    public Integer getIDSales();
+    public void setIDSales(Integer IDSales);
+
+    public Integer getCode();
+    public void setCode(Integer Code);
+
+    public String getNameSales();
+    public void setNameSales(String NameSales);
+
+    public Double getPriceSales();
+    public void setPriceSales(Double PriceSales);
+
+    public Double getPrice();
+    public void setPrice(Double Price);
+
+}
