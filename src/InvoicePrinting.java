70f0abf4ec339ed89299241049c7e431979554e3
TheJagermeister <pedroservano007@gmail.com>
2022-11-27 02:19:37 +0100

    Actualizacion de diseño de la factura

--- src/InvoicePrinting.java
+++ src/InvoicePrinting.java
@@ -3,33 +3,31 @@ public class InvoicePrinting {
 
 
     public void print(Invoice invoice) {
-        System.out.println("Codigo de factura: " + invoice.getIdInv() + "          " +
-                "       " + invoice.getDate());
+        System.out.println("Codigo de factura: " + invoice.getIdInv() + " | " + invoice.getDate());
         if (invoice.getAutonomous() != null) {
-            System.out.println("ID: " + invoice.getAutonomous().getCodeCustomer() + "\n" +
-                    "Nombre: " + invoice.getAutonomous().getNameCust() +
-                    "Apellidos: " + invoice.getAutonomous().getSurnameAutonomous() + "\n" +
-                    "Direccion: " + invoice.getAutonomous().getAddress() + "        Ciudad: " + invoice.getAutonomous().getCity() + "       Provincia: " +
-                    invoice.getAutonomous().getProvince() + "\n" +
-                    "Correo electronico: " + invoice.getAutonomous().getEmail());
+            System.out.println("CIF/NIF: " + invoice.getAutonomous().getCodeCustomer() + " | Nombre: " + invoice.getAutonomous().getNameCust() +
+                    " | Apellidos: " + invoice.getAutonomous().getSurnameAutonomous() +
+                    " | Direccion: " + invoice.getAutonomous().getAddress() + " | Poblacion: " + invoice.getAutonomous().getCity() + " | Provincia: " +
+                    invoice.getAutonomous().getProvince() +
+                    " | Email: " + invoice.getAutonomous().getEmail());
         } else{
-        System.out.println("ID: " + invoice.getSociety().getCodeCustomer() + "\n" +
-                "Nombre: " + invoice.getSociety().getNameCust() + "\n" +
-                "Direccion: " + invoice.getSociety().getAddress() + "        Ciudad: " + invoice.getSociety().getCity() + "       Provincia: " +
-                invoice.getSociety().getProvince() + "\n" +
-                "Correo electronico: " + invoice.getSociety().getEmail());
+        System.out.println("CIF/NIF: " + invoice.getSociety().getCodeCustomer() +
+                " | Nombre: " + invoice.getSociety().getNameCust() +
+                " | Direccion: " + invoice.getSociety().getAddress() + " | Email: " + invoice.getSociety().getCity() + " | Provincia: " +
+                invoice.getSociety().getProvince() +
+                " | Email: " + invoice.getSociety().getEmail());
         }
 
             System.out.println("Productos:");
-            System.out.println("ID: " + invoice.getProduct().getIdSales() + " | " + invoice.getProduct().getNameProd() + " | " +
-                    invoice.getProduct().getPrice() + "€" + " | " + invoice.getProduct().getIvaType().getType() * 100 + "%" + " | " +
-                    (invoice.getProduct().getPrice() + invoice.getProduct().getPrice() * invoice.getProduct().getIvaType().getType()));
+            System.out.println("Codigo: " + invoice.getProduct().getIdSales() + " | Nombre: " + invoice.getProduct().getNameProd() + " |  Precio: " +
+                    invoice.getProduct().getPrice() + "€" + " | Tipo IVA: " + invoice.getProduct().getIvaType().getType() * 100 + "%" + " | Total: " +
+                    (invoice.getProduct().getPrice() + invoice.getProduct().getPrice() * invoice.getProduct().getIvaType().getType()) + "€");
             System.out.println("Servicios:");
-            System.out.println("ID: " + invoice.getService().getIdSales() + " | " + invoice.getService().getNameSales() + " | " +
-                    invoice.getService().getPrice() + "€" + " | " + invoice.getService().getIvaType().getType() * 100 + "%" + " | " +
-                    (invoice.getService().getPrice() + invoice.getService().getPrice() * invoice.getService().getIvaType().getType()));
+            System.out.println("Codigo: " + invoice.getService().getIdSales() + " | Nombre: " + invoice.getService().getNameSales() + " | Precio:" +
+                    invoice.getService().getPrice() + "€" + " | Tipo IVA: " + invoice.getService().getIvaType().getType() * 100 + "%" + " | Total: " +
+                    (invoice.getService().getPrice() + invoice.getService().getPrice() * invoice.getService().getIvaType().getType()) + "€");
 
-        System.out.println("Total: " + (invoice.getProduct().getPrice() + invoice.getProduct().getPrice() * invoice.getProduct().getIvaType().getType() +
-                invoice.getService().getPrice() + invoice.getService().getPrice() * invoice.getService().getIvaType().getType()));
+        System.out.println("Total: " + Math.floor((invoice.getProduct().getPrice() + invoice.getProduct().getPrice() * invoice.getProduct().getIvaType().getType() +
+                invoice.getService().getPrice() + invoice.getService().getPrice() * invoice.getService().getIvaType().getType())) + "€");
     }
 }
