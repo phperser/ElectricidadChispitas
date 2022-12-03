965007a26c49256808bfea0c4ea153c70a94fa70
TheJagermeister <pedroservano007@gmail.com>
2022-12-03 05:09:43 +0100

    Actualizacion de interfaces.

---
+++ src/com/phpeser/chispas/domain/models/Customer.java
@@ -0,0 +1,27 @@
+package com.phpeser.chispas.domain.models;
+
+public interface Customer {
+
+    public String getCodeCustomer();
+    public void setCodeCustomer(String codeCustomer);
+
+    public String getNameCust();
+    public void setNameCust(String nameCust);
+
+    public String getAddress();
+    public void setAddress(String address);
+
+    public String getCity();
+    public void setCity(String city);
+
+    public String getProvince();
+    public void setProvince(String province);
+
+    public String getEmail();
+    public void setEmail(String email);
+
+    public Integer getPhoneNumber();
+    public void setPhoneNumber(Integer phoneNumber);
+
+
+}
